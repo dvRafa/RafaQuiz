30c9d536a2565a65baaea492e05df1047e4b1bd7
rafadinero22 <rafaeljesus694@outlook.com>
2023-01-25 15:18:52 -0500

    feat: new quetions to deploy to users

--- src/assets/Questions.js
+++ src/assets/Questions.js
@@ -1,4 +1,4 @@
-export const Questions = [
+export const levelOne = [
   {
     id: 0,
     questionText: 'Which acient civilization built the Great Pyramids?',
@@ -11,7 +11,7 @@ export const Questions = [
   },
   {
     id: 1,
-    questionText: 'Who was the leader of the Soviet Union during World War ||?',
+    questionText: 'Who was the leader of the Soviet Union during World War II?',
     answerOptions: [
       { answerText: 'Joseph Stalin', isCorrect: true },
       { answerText: 'Vladimir Putin', isCorrect: false },
@@ -29,35 +29,35 @@ export const Questions = [
       { answerText: 'All of the above', isCorrect: true }
     ]
   },
-  {
-    id: 3,
-    questionText:
-      'How long does it take for light to travel from the sun to Earth?',
-    answerOptions: [
-      { answerText: '8 minutes', isCorrect: true },
-      { answerText: '12 minute', isCorrect: false },
-      { answerText: '20 minute', isCorrect: false },
-      { answerText: '24 minute', isCorrect: false }
-    ]
-  },
-  {
-    id: 5,
-    questionText: 'What is the largest planet in our solar system?',
-    answerOptions: [
-      { answerText: 'Jupiter', isCorrect: true },
-      { answerText: 'Saturn', isCorrect: false },
-      { answerText: 'Uranus', isCorrect: false },
-      { answerText: 'Neptune', isCorrect: false }
-    ]
-  },
-  {
-    id: 6,
-    questionText: 'What is the most popular music genre in the world?',
-    answerOptions: [
-      { answerText: 'Pop', isCorrect: false },
-      { answerText: 'Hip-Hop', isCorrect: false },
-      { answerText: 'Rock', isCorrect: false },
-      { answerText: 'Classical', isCorrect: false }
-    ]
-  }
 ]
+const levelTwo = [{
+  id: 3,
+  questionText:
+    'How long does it take for light to travel from the sun to Earth?',
+  answerOptions: [
+    { answerText: '8 minutes', isCorrect: true },
+    { answerText: '12 minute', isCorrect: false },
+    { answerText: '20 minute', isCorrect: false },
+    { answerText: '24 minute', isCorrect: false }
+  ]
+},
+{
+  id: 5,
+  questionText: 'What is the largest planet in our solar system?',
+  answerOptions: [
+    { answerText: 'Jupiter', isCorrect: true },
+    { answerText: 'Saturn', isCorrect: false },
+    { answerText: 'Uranus', isCorrect: false },
+    { answerText: 'Neptune', isCorrect: false }
+  ]
+},
+{
+  id: 6,
+  questionText: 'What is the most popular music genre in the world?',
+  answerOptions: [
+    { answerText: 'Pop', isCorrect: false },
+    { answerText: 'Hip-Hop', isCorrect: false },
+    { answerText: 'Rock', isCorrect: false },
+    { answerText: 'Classical', isCorrect: false }
+  ]
+}]
