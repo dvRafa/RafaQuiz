6cc497624f04eda50837315f3db88f474b0b548a
rafadinero22 <rafaeljesus694@outlook.com>
2023-02-01 20:10:17 -0500

    feat: add key prop

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,21 +2,18 @@ import { Link } from 'wouter'
 
 const LEVELS_URL = [
   {
-    route: '/vol-page',
-    label: 'Easy'
-  },
-  {
-    route: '/vol-page-2',
-    label: 'Moderate'
+    id:10,
+    label: 'Easy',
+    route: '/EasyMode'
   }
 ]
 
-export default function MenuComponent() {
+export default function Menu() {
   return (
     <div className='grid place-content-center max-w-2xl gap-y-10'>
       {LEVELS_URL.map((level) => (
-        <nav className='grid grid-col'>
-          <Link href={level.route}>
+        <nav key={level.id} className='grid grid-col'>
+          <Link href={level.route} >
             <a className='text-center text-2xl font-mono font-bold border-2 border-[#222] py-4 px-6 rounded-full hover:border-[#444]'>{level.label}</a>
           </Link>
         </nav>
