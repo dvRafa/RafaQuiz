a69252123c1332c61986db93925e0afa095db195
rafadinero22 <rafaeljesus694@outlook.com>
2023-02-03 00:28:49 -0500

    feat: add new llink

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,19 +2,26 @@ import { Link } from 'wouter'
 
 const LEVELS_URL = [
   {
-    id:10,
+    id: Math.random(),
     label: 'Easy',
     route: '/EasyMode'
+  },
+  {
+    id: Math.random(),
+    label: 'Moderate',
+    route: '/ModerateMode'
   }
 ]
 
 export default function Menu() {
   return (
-    <div className='grid place-content-center max-w-2xl gap-y-10'>
+    <div className="grid place-content-center max-w-2xl gap-y-10">
       {LEVELS_URL.map((level) => (
-        <nav key={level.id} className='grid grid-col'>
-          <Link href={level.route} >
-            <a className='text-center text-2xl font-mono font-bold border-2 border-[#222] py-4 px-6 rounded-full hover:border-[#444]'>{level.label}</a>
+        <nav key={level.id} className="grid grid-col">
+          <Link href={level.route}>
+            <a className="text-center text-2xl font-mono font-bold border-2 border-[#222] py-4 px-6 rounded-full hover:border-[#444]">
+              {level.label}
+            </a>
           </Link>
         </nav>
       ))}
