cf92475b1e63d1aca8d5a6ded5347f0f8639ae30
rafadinero22 <rafaeljesus694@outlook.com>
2023-02-03 00:27:17 -0500

    feat: add new route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './global.css'
 import { Route } from 'wouter'
 import Menu from 'components/Menu'
-import EasyMode from 'components/EasyMode'
+import ModerateMode from 'components/Modes/ModerateMode'
+import EasyMode from 'components/Modes/EasyMode'
 
 function App() {
   return (
     <main>
       <Route path="/" component={Menu} />
       <Route path="/EasyMode" component={EasyMode} />
+      <Route path="/ModerateMode" component={ModerateMode} />
     </main>
   )
 }
