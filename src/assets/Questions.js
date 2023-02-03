1ddea5ccfab146058f1cdba635cd6c6de79f16a4
rafadinero22 <rafaeljesus694@outlook.com>
2023-02-03 00:28:22 -0500

    feat: uncommented moderate mode questions

--- src/assets/Questions.js
+++ src/assets/Questions.js
@@ -28,36 +28,38 @@ export const EasyModeQuestions = [
       { answerText: 'Slavery', isCorrect: false },
       { answerText: 'All of the above', isCorrect: true }
     ]
-  },
+  }
 ]
-// export const Moderate_Mode = [{
-//   id: 3,
-//   questionText:
-//     'How long does it take for light to travel from the sun to Earth?',
-//   answerOptions: [
-//     { answerText: '8 minutes', isCorrect: true },
-//     { answerText: '12 minute', isCorrect: false },
-//     { answerText: '20 minute', isCorrect: false },
-//     { answerText: '24 minute', isCorrect: false }
-//   ]
-// },
-// {
-//   id: 5,
-//   questionText: 'What is the largest planet in our solar system?',
-//   answerOptions: [
-//     { answerText: 'Jupiter', isCorrect: true },
-//     { answerText: 'Saturn', isCorrect: false },
-//     { answerText: 'Uranus', isCorrect: false },
-//     { answerText: 'Neptune', isCorrect: false }
-//   ]
-// },
-// {
-//   id: 6,
-//   questionText: 'What is the most popular music genre in the world?',
-//   answerOptions: [
-//     { answerText: 'Pop', isCorrect: true },
-//     { answerText: 'Hip-Hop', isCorrect: false },
-//     { answerText: 'Rock', isCorrect: false },
-//     { answerText: 'Classical', isCorrect: false }
-//   ]
-// }]
+export const ModerateModeQuestions = [
+  {
+    id: 3,
+    questionText:
+      'How long does it take for light to travel from the sun to Earth?',
+    answerOptions: [
+      { answerText: '8 minutes', isCorrect: true },
+      { answerText: '12 minutes', isCorrect: false },
+      { answerText: '20 minutes', isCorrect: false },
+      { answerText: '24 minutes', isCorrect: false }
+    ]
+  },
+  {
+    id: 5,
+    questionText: 'What is the largest planet in our solar system?',
+    answerOptions: [
+      { answerText: 'Jupiter', isCorrect: true },
+      { answerText: 'Saturn', isCorrect: false },
+      { answerText: 'Uranus', isCorrect: false },
+      { answerText: 'Neptune', isCorrect: false }
+    ]
+  },
+  {
+    id: 6,
+    questionText: 'What is the most popular music genre in the world?',
+    answerOptions: [
+      { answerText: 'Pop', isCorrect: true },
+      { answerText: 'Hip-Hop', isCorrect: false },
+      { answerText: 'Rock', isCorrect: false },
+      { answerText: 'Classical', isCorrect: false }
+    ]
+  }
+]
